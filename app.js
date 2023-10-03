baea935be35b816e0054f1ece0289612039da468
Dóra Tokai <tokaidora88@gmail.com>
2023-10-03 15:01:29 +0300

    Add data form PokeAPI

---
+++ app.js
@@ -0,0 +1,54 @@
+const cardContainer = document.querySelector(".cards");
+
+let pokeData;
+
+const fetchData = async () => {
+  await fetch("https://pokeapi.co/api/v2/pokemon?limit=121&offset=0")
+    .then((response) => response.json())
+    .then((data) => {
+      const fetches = data.results.map((item) => {
+        return fetch(item.url)
+          .then((res) => res.json())
+          .then((data) => {
+            return {
+              id: data.id,
+              name: data.name,
+              img: data.sprites.other["official-artwork"].front_default,
+              types: data.types,
+            };
+          });
+      });
+
+      Promise.all(fetches).then((res) => {
+        pokeData = res;
+        pokeCards();
+        console.log(pokeData);
+      });
+    });
+};
+
+const pokeCards = () => {
+  const content = pokeData
+    .map((pokemon) => {
+      return `
+        <div class="card">
+            <p class="card__id">#${pokemon.id}</p>
+            <div class="card__img">
+                <img src=${pokemon.img} alt="Pikachu" />
+            </div>
+            <div class="card__info">
+                <h3 class="card__title">${pokemon.name}</h3>
+                <p class="card__tags">
+                
+                <span class="tag electric">Electric</span><span class="tag poison">Poison</span>
+                </p>
+            </div>
+        </div>
+    `;
+    })
+    .join("");
+
+  cardContainer.innerHTML = content;
+};
+
+fetchData();
