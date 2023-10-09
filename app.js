ccfe98b1cd35a9252141bf0d73d674c58fb9b58d
tdora28 <tokaidora88@gmail.com>
2023-10-09 15:01:10 +0300

    Add height and weight info to cards

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const cardContainer = document.querySelector(".cards");
-const searchBar = document.querySelector("#search");
+const cardContainer = document.querySelector('.cards');
+const searchBar = document.querySelector('#search');
 
 console.log(searchBar.value);
 
 let pokeData = [];
 
 const fetchData = async () => {
-  await fetch("https://pokeapi.co/api/v2/pokemon?limit=121&offset=0")
+  await fetch('https://pokeapi.co/api/v2/pokemon?limit=121&offset=0')
     .then((response) => response.json())
     .then((data) => {
       const fetches = data.results.map((item) => {
@@ -16,15 +16,17 @@ const fetchData = async () => {
             return {
               id: data.id,
               name: data.name,
-              img: data.sprites.other["official-artwork"].front_default,
+              img: data.sprites.other['official-artwork'].front_default,
               types: data.types,
+              height: data.height,
+              weight: data.weight,
             };
           });
       });
 
       Promise.all(fetches).then((res) => {
         pokeData = res;
-        pokeCards("");
+        pokeCards('');
       });
     });
 };
@@ -49,20 +51,24 @@ const pokeCards = (searchString) => {
             <div class="card__info">
                 <h3 class="card__title">${pokemon.name}</h3>
                 <p class="card__tags">
-                  ${tags.join("")}
+                  ${tags.join('')}
+                </p>
+                <p class="card__measurements">
+                <span>Height: ${(pokemon.height / 10).toFixed(1)} m</span>
+                <span>Weight: ${(pokemon.weight / 10).toFixed(1)} kg</span>
                 </p>
             </div>
         </div>
      `;
     })
-    .join("");
+    .join('');
 
   cardContainer.innerHTML = content;
 };
 
 fetchData();
 
-searchBar.addEventListener("input", (e) => {
+searchBar.addEventListener('input', (e) => {
   // The "input" event will work also when clearing the searchBar (maybe better than "keyup")
   const searchString = e.target.value.toLowerCase();
   pokeCards(searchString);
